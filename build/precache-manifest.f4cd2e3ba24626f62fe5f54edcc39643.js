d3103a530077293e4100a394c840cdb8909fd7c5
DmytroLukianenko <71637913+DmytroLukianenko@users.noreply.github.com>
2021-02-03 21:10:03 +0100

    npm run build

---
+++ build/precache-manifest.f4cd2e3ba24626f62fe5f54edcc39643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf182d5a3ff91e2ed5947c4424984fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e2ec591a038f1469993",
+    "url": "/static/css/main.334e9c2d.chunk.css"
+  },
+  {
+    "revision": "df466658a6e7aab5e050",
+    "url": "/static/js/2.483bb0fe.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.483bb0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2ec591a038f1469993",
+    "url": "/static/js/main.0956d93e.chunk.js"
+  },
+  {
+    "revision": "3f1000a624e229863dde",
+    "url": "/static/js/runtime-main.ae092f30.js"
+  }
+]);
